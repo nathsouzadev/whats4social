64891e53092fc61ffaed321a5fd53a5ddc859924
Nathally Souza <nathallyccd@gmail.com>
2024-05-04 08:31:58 -0300

    [fix]add SocialService to MessageModule

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -4,9 +4,10 @@ import { MessageController } from './message.controller';
 import { TwitterService } from '../social/client/twitter.service';
 import { BSkyService } from '../social/client/bsky.service';
 import { MetaService } from '../social/client/meta.service';
+import { SocialService } from '../social/services/social.service';
 
 @Module({
   controllers: [MessageController],
-  providers: [MessageService, TwitterService, BSkyService, MetaService],
+  providers: [MessageService, SocialService, TwitterService, BSkyService, MetaService],
 })
 export class MessageModule {}
