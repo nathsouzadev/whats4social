5daa521feaa27af307ec10f503f666d23927e77b
Nathally Souza <nathallyccd@gmail.com>
2024-05-04 18:47:14 -0300

    [chore] .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
