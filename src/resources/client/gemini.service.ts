404aae5b1957ad76c64a553f1a510c13e0758a26
Nathally Souza <nathallyccd@gmail.com>
2024-08-30 07:29:36 -0300

    audio buffer

--- src/resources/client/gemini.service.ts
+++ src/resources/client/gemini.service.ts
@@ -5,5 +5,5 @@ export class GeminiService {
   private logger = new Logger(GeminiService.name);
 
   sendAudio = (audio: Express.Multer.File) =>
-    this.logger.log(`Audio received ${audio.fieldname}`);
+    this.logger.log(`Audio received ${audio.buffer}`);
 }
